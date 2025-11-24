850bcdda312b8cf9cea9cef3cb00c89dc4ad46a8
Mina Nabil <minanabil2004@gmail.com>
2025-11-24 03:03:04 +0200

    adjust the ai player boolean

--- backend/app.py
+++ backend/app.py
@@ -41,7 +41,7 @@ def solve():
     else:
         return jsonify({"error": "Unknown algorithm"}), 400
 
-    board = board.apply_action(best_col, not ai_player)
+    board = board.apply_action(best_col, ai_player)
     print(board)
 
     response_data = {
@@ -50,8 +50,8 @@ def solve():
         "value": best_val,
         "nodes_expanded": nodes,
         "tree": root.to_json(),
-        "AiScore": board.count_connected(False),
-        "HumanScore": board.count_connected(True)
+        "AiScore": board.count_connected(True),
+        "HumanScore": board.count_connected(False)
     }
 
     # print(json.dumps(response_data, indent=4))
